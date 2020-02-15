1d2a2fbe39523193ab3a3c830874488cddcc496a
John Cornes <john.h.cornes@gmail.com>
2020-02-15 08:21:41 +0000

    Add functionality to make_reading

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ FORMAT = {
 	'name_chinese' : {'options' : {'set_alignment' : ALIGNMENTS.M, 'set_bold': True, 'space' : True}},
 	'judgement' : {'options' : {'space' : True}, 'title' : 'Judgement'},
 	'image' : {'options' : {'space' : True}, 'title' : 'Image'},
-	'lines' : {'options' : {'set_alignment' : ALIGNMENTS.M, 'set_row_spacing':0}, 'space' : True},
-	'emph_lines' : {'options' : {'set_bold':True}, 'title' : "Emphasized Lines", 'space': True},
-	'normal_lines' : {'options' : {}, 'title' : "Normal Lines", 'space' : True},
+	'lines' : {'options' : {'set_alignment' : ALIGNMENTS.M, 'set_row_spacing':0, 'space' : True}},
+	'emph_lines' : {'options' : {'set_bold':True, 'space': True}, 'title' : "Emphasized Lines"},
+	'normal_lines' : {'options' : {'space' : True}, 'title' : "Normal Lines"},
 	'default' : {'options':{'space' : True}},
 	'chinese_defualt' : 
 		{'options':{'space' : True, 'set_chinese_mode' : True, 'set_chinese_format' : CHINESE.UTF8}},
@@ -39,8 +39,11 @@ def format_reading(reading):
 	def make_line(line, options):
 		line_template = {"type" : "text", "info" : '', "options" : {}}
 
-		if is_chinese(line):
-			line_template.update(FORMAT['chinese_defualt'])
+		try:
+			if is_chinese(line):
+				line_template.update(FORMAT['chinese_defualt'])
+		except:
+			print("Uh oh this caused a woopsy poopsy error: {}".format(line))
 
 		line_template['info'] = line
 		line_template['options'].update(options)
@@ -61,7 +64,7 @@ def format_reading(reading):
 
 		if k is "lines":
 			for line in v:
-				print_data.append(make_line(YiJingLineTrans[line], options))
+				print_data.append(make_line(hexagrams.YiJingLineTrans[line], options))
 
 		elif k is "emph_lines" or k is "normal_lines":
 			for line in v:
@@ -75,12 +78,15 @@ def format_reading(reading):
 
 	return print_data
 
-def make_reading(hexa):
+def make_reading(h):
+
 	r_hex = {}
-	info = hexa.info
+	info = h.info
 
 	r_hex['name'] = info['name']
 	r_hex['name_chinese'] = info['name_chinese']
+
+	r_hex['lines'] = [x.val for x in h.lines]
 
 	r_hex['judgement'] = info['judgement']
 	r_hex['judgement_chinese'] = info['judgement_chinese']
@@ -88,14 +94,32 @@ def make_reading(hexa):
 	r_hex['image'] = info['image']
 	r_hex['image_chinese'] = info['image_chinese']
 
+
+	if h.moving:
+		r_hex['normal_lines'] = []
+
+	if h.moving > 1 and h.moving < 6:
+		r_hex['emph_lines'] = []
+
+	if h.moving == 1:
+		r_hex['normal_lines'].append(info['line{}'.format(h.moving_pos[0])])
+	elif h.moving == 2:
+		r_hex['emph_lines'].append(info['line{}'.format(h.moving_pos[1])])
+		r_hex['normal_lines'].append(info['line{}'.format(h.moving_pos[0])])
+	elif h.moving == 3:
+		pass
+
 	return r_hex
 
 
 if __name__ == '__main__':
 	hexagram = hexagrams.YijingHexagram()
 
+	while hexagram.moving != 1:
+		hexagram = hexagrams.YijingHexagram()
+
 	formatted_reading = format_reading(make_reading(hexagram))
 
-	p = DFR0503()
-	print_data(formatted_reading, p)
-	p.feed(2)
+	# p = DFR0503()
+	# print_data(formatted_reading, p)
+	# p.feed(2)
